62705451931542d8a348e0f9e3af841910806567
Juan Gálvez <juangalvezccopa@gmail.com>
2023-05-24 10:27:36 -0500

    Changes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-var randomTexts = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    // Function to generate a random index
+const randomTexts = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// Function to generate a random index
 function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
-
-    // Function to display a random text
+// Function to display a random text
 function displayRandomText() {
-    var randomIndex = getRandomIndex(randomTexts.length);
-    var randomText = randomTexts[randomIndex];
+    const randomIndex = getRandomIndex(randomTexts.length);
+    const randomText = randomTexts[randomIndex];
     document.getElementById("random-text").textContent = randomText;
 }
 // Call the displayRandomText function when the page loads
