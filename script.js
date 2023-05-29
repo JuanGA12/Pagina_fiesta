52df7aa0dd0b8f7fb3d721720840e7947d47bbaa
Juan Gálvez <juangalvezccopa@gmail.com>
2023-05-29 18:17:16 -0500

    styles

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const randomTexts = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const texts = [
+    "Yo detecto y reporto el Phishing con el botón de Phish Alert",
+    "Yo bloqueo mi laptop cuando no la uso (Windows + L)",
+    "Mi contraseña es robusta: Am0elcevich3!"
+]
 // Function to generate a random index
 function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
 // Function to display a random text
 function displayRandomText() {
-    const randomIndex = getRandomIndex(randomTexts.length);
-    const randomText = randomTexts[randomIndex];
+    const randomIndex = getRandomIndex(texts.length);
+    const randomText = texts[randomIndex];
     document.getElementById("random-text").textContent = randomText;
 }
 // Call the displayRandomText function when the page loads
